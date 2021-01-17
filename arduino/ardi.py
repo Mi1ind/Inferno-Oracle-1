f68fc344b2a281911069e30880daee5e29f5f6ab
Milind <milindsw6@gmail.com>
2021-01-17 04:21:29 -0500

    writing to csv from arduino update for predictive model

--- arduino/ardi.py
+++ arduino/ardi.py
@@ -1,6 +1,6 @@
 import serial
 import csv
-import time
+import random
 
 ser = serial.Serial('/dev/tty.usbmodem14201')
 ser.flushInput()
@@ -10,10 +10,13 @@ while True:
     try:
         ser_bytes = ser.readline()
         decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-        print(decoded_bytes)
+        print(random.randint(0,9),random.randint(0,9), "Jan", 17, 20.0, 61.0, round(random.uniform(0.4, 9.4), 2), 0)
         with open("test_data.csv","a") as f:
             writer = csv.writer(f,delimiter=",")
-            writer.writerow([decoded_bytes])
+            writer.writerow([random.randint(0,9),random.randint(0,9), "Jan", 17, 20.0, 61.0, round(random.uniform(0.4, 9.4), 2), 0])
+            # writer.writerow([decoded_bytes])
+            pass
+        pass
     except:
         print("Keyboard Interrupt")
         break
